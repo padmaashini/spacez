8dfbe5d88b64a9ab73af6f2674d5cd176d2bc578
qeenyra <qeenyra@gmail.com>
2022-04-11 22:34:15 -0400

    set up login and signup screens

---
+++ views/login/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+//styles
+import styles from './login.styles';
+
+const Login = () => {
+
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default Login; 
---
+++ views/signup/signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+//styles
+import styles from './signup.styles';
+
+const Signup = () => {
+
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+export default Signup; 
